db970856c36e4445ee260d19e833bc1865022cd3
Daniel Herzegh <danhe178@student.liu.se>
2019-04-08 21:12:01 +0200

    add code to lab2

---
+++ lab2/app/src/main/java/com/example/lab2/Content.java
@@ -0,0 +1,10 @@
+package com.example.lab2;
+
+public class Content {
+    static String[] Info = {
+            "Sweden\n\n bla bla",
+            "Norway\n\n blaalalal",
+            "Squarepants\n\n sjishfd",
+            "wasduhasoduhguod"
+    };
+}
---
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.lab2;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity
+        implements GroupListFragment.OnGroupListSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info_about_stuff);
+
+        if (findViewById(R.id.fragment_container) != null) {
+
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            GroupListFragment firstFragment = new GroupListFragment();
+
+            firstFragment.setArguments(getIntent().getExtras());
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
+        }
+
+    }
+
+    public void onObjectSelected(int position) {
+        InfoFragment infoFrag = (InfoFragment)
+                getSupportFragmentManager().findFragmentById(R.id.info_fragment);
+        if (infoFrag != null) {
+
+            infoFrag.updateInfoView(position);
+        }
+        else {
+            InfoFragment newFragment = new InfoFragment();
+            Bundle args = new Bundle();
+            args.putInt(InfoFragment.ARG_POSITION, position);
+            newFragment.setArguments(args);
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            transaction.replace(R.id.fragment_container, newFragment);
+            transaction.addToBackStack(null);
+
+            transaction.commit();
+
+        }
+    }
+}
